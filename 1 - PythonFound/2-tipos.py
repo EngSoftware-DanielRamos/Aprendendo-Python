c96f7c5efc830afb44528779811142d0868b3535
Daniel Ramos <financdaniel@gmail.com>
2024-11-18 16:28:25 -0300

    Python I - Introdução ao Python

---
+++ 1 - PythonFound/2-tipos.py
@@ -0,0 +1,14 @@
+name = "Resident Evil 4 Remake"
+yearLaunch = 2023
+gamePrince = 300.00
+planIncluded = False
+
+print(name)
+print(yearLaunch)
+print(gamePrince)
+print(planIncluded)
+
+print(type(name))
+print(type(yearLaunch))
+print(type(gamePrince))
+print(type(planIncluded))
