542a3985de0a5830aec20fdc74da131eb2571303
Daniel Ramos <financdaniel@gmail.com>
2024-11-25 17:11:29 -0300

    Modulo 2 finalizado

---
+++ 2 - Modulos/exercicio4_enunciado.py
@@ -0,0 +1,7 @@
+"""
+* Advinhe o número
+-> Escreva um programa em Python que gera um número aleatório para que o usuário tente acertar o número. Agumas sugestões para o programa:
+1 - Utilizar um laço de repetição para que o programa execute at[e que o usuário informe um número referente a opção para sair do programa.
+2 - Utilizar o módulo random para gerar valores aleatórios dentro de um intervalo. Ex: 1 a 10.
+3 - Lê o número que o usuário digitar via input e comparar se é o mesmo número que o programa sorteou.
+"""
